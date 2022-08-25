c007fedbe9db0c6fc1ef3ac5e0374388eaf4bab5
Diemisom Melo <dcarlospa@gmail.com>
2022-08-25 20:09:34 -0300

    aula 1

--- src/app/explore-container/explore-container.component.ts
+++ src/app/explore-container/explore-container.component.ts
@@ -7,9 +7,21 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class ExploreContainerComponent implements OnInit {
   @Input() name: string;
+
+  i = 0;
+  j=0;
+  private url = "https://ionicframework.com/docs/components";
   
   constructor() { }
 
   ngOnInit() {}
+
+  meuAlert(){
+    console.log(++this.i, 'Clicou no botão')
+  }
+
+  meuAlertA(){
+    console.log(++this.j, 'Clicou no Strong')
+  }
 
 }
