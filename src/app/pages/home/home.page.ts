f6f3a1bf66b0aaa7425fbb088d84576147c50b70
Diemisom Melo <dcarlospa@gmail.com>
2022-08-30 21:26:09 -0300

    add ação de login e register page

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePage implements OnInit {
 
-  constructor() { }
+  constructor(
+    private gerenciadorDeRotas: Router
+  ) { }
 
   ngOnInit() {
+  }
+
+  sair(){
+    /*
+    Executa toda a lógica de sair, apagando dados do usuário, etc...
+    Navega para a página de login
+    */
+    console.log('Saindo..');
+    this.gerenciadorDeRotas.navigateByUrl('login');
   }
 
 }
--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,14 +8,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  email: string = '';
+  senha = '';
+  private senhaMestre = "123";
+
+  constructor(
+    private router: Router
+  ) { 
+    console.log(router.url);
+  }
 
   ngOnInit() {
   }
 
   entrar(){
     console.log('entrando...');
-    window.open('http://localhost:8100/home')
+    console.log(this.email, this.senha);
+    if(this.validateEmail(this.email) && this.senha === this.senhaMestre)
+      this.router.navigateByUrl('home');
+    else
+      alert('Dados incorretos');
+  }
+
+  validateEmail(email) {
+    return String(email)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      );
   }
 
 }
